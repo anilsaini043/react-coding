af0670620f345e57bc2fb5554dbc69e219d7818d
anilsaini043 <anilsaini043@gmail.com>
2026-03-01 23:17:25 +0530

    Accordion folder structure changed

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import "./accordion.css";
 import { HiChevronUp, HiChevronDown } from "react-icons/hi";
 
 const Accordion = ({items}) => {
